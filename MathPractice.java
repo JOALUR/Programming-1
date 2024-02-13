21cd1cf5d94ff3e4ba5de6bc5c6ae60339434eed
Jose Uribe <116236557+JOALUR@users.noreply.github.com>
2024-02-13 12:14:51 -0500

    Add files via upload

---
+++ MathPractice.java
@@ -0,0 +1,37 @@
+
+public class MathPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//nt x = 14;
+		//int y = 13;
+		//int a = 1;
+		//int b = 3;
+		//int c = 5;
+		//int equation = ((3+4*x)/5)-(10*(y-5)*(a+b+c))/x+9*((4/x)+((9+x)/y));
+		
+		//System.out.println(equation);
+		
+		//int x = 3;
+		//int y = 5;
+		//int z = 8;
+
+        //int m = x++   -   --y   +  ++z;
+        //System.out.println(m);
+        
+        int n = 3;
+        int q = 4;
+        int p = 2;
+        q *= q + n*p + q;
+        
+        System.out.println(q);
+        
+        int d = 9, e = 12;
+        System.out.printf("%d %d\n", d, e);
+        
+        double x = 12.3798146;
+        System.out.printf("%.2f\n", x);
+	}
+
+}
